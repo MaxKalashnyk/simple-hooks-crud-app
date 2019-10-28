668ff80d548e0c0f1990293a4d4e1ec289065677
Maksym Kalashnyk <maksym.kalashnyk@uptech.team>
2019-10-28 19:19:19 +0200

    Updates

---
+++ precache-manifest.ef9e703bb550a42abdf5d890851f995a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d41efe7ac15c47ce01ada5cf45aefb36",
+    "url": "/simple-hooks-crud-app/index.html"
+  },
+  {
+    "revision": "4ff632927287613cc812",
+    "url": "/simple-hooks-crud-app/static/css/main.88eaaa6d.chunk.css"
+  },
+  {
+    "revision": "269e7f9c1d919cc2ac09",
+    "url": "/simple-hooks-crud-app/static/js/2.3131b5cd.chunk.js"
+  },
+  {
+    "revision": "4ff632927287613cc812",
+    "url": "/simple-hooks-crud-app/static/js/main.2e8600a1.chunk.js"
+  },
+  {
+    "revision": "70f86a0f6deb70013c4a",
+    "url": "/simple-hooks-crud-app/static/js/runtime-main.44640e3e.js"
+  }
+]);
